1b11ec0a4d53da2d33488062b9cbfe9d4328ba89
zekunzhan <zekunzhan@tencent.com>
2023-03-17 14:20:34 +0800

    feat: add antdv1.x

--- packages/demo/demo-v2/src/pages/index/index.js
+++ packages/demo/demo-v2/src/pages/index/index.js
@@ -6,7 +6,8 @@ import 'demo-common/bootstrap.js';
 import Vue from 'vue';
 import VueRouter from 'VueRouter';
 import elementUI from 'demo-common/components/ElementUi/index.js';
-
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import routes from './routes';
 
 import App from './App';
@@ -14,7 +15,7 @@ import App from './App';
 import './style.css';
 
 Vue.use(VueRouter);
-
+Vue.use(Antd);
 // Ui
 Vue.use(elementUI);
 
@@ -28,3 +29,4 @@ new Vue({
     }),
     render: h => h(App)
 }).$mount('#app');
+s
